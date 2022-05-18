269b4cb29f9a515dfaf12f6cccc5ce492d3d34ca
Violeta Vinogrodska <105384019+Wiolla55525@users.noreply.github.com>
2022-05-18 23:32:54 +0300

    commit on main

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ for(let item of countries) {
     const atsakymas = `Salis: ${country}, joje gyvena ${gyventojai}mln. gyventoju, valstybes plotas - ${plotas} km^2, plotas, tenkantis vienam gyventojui - ${plotasGyventojui} m^2 </br> `
     document.write(atsakymas);
 }
+
+
 */
+
 
 /*const doors = {
     key: 'secretKey',
@@ -50,6 +53,50 @@ console.log(doors.open());
 
 */
 
+/*
+const cars = {
+    type: "BMW", price: '30.000',
+    type: "MERCEDES", price: '35.000',
+    type: "HONDA", price: '18.000',
+    type: "VOLVO", price: '25.000',
+    obj:{}
+
+    .forEach(myFunction),
+
+    function myFunction(type) {
+        return 
+        `${type} price is $${price}`
+    }
+
+}
+console.log(typeof cars);
+*/
+
+const cars = [
+    {type: "BMW", price: '30.000'},
+    {type: "MERCEDES", price: '35.000'},
+    {type: "HONDA", price: '18.000'},
+    {type: "VOLVO", price: '25.000'},
+];
+
+cars.forEach(function(cars))
+cars.forEach(type => console.log(cars.type));
+forEach(function(type))
+
+const carsType = cars.type;
+cars.type
+/*
+`${type} car price $${price}`
+cars.forEach(myFunction);
+
+function myFunction(item) {
+    return 
+    `${type} price is $${price}`
+}
+console.log(typeof cars)
+*/
+
+/*
 let savingsAccount = {
     balance: 1000,
     interestRatePercent: 1,
@@ -58,12 +105,25 @@ let savingsAccount = {
             savingsAccount.balance += amount;
         }
     },
-    withdraw: function removeMoney (amount) {
+   /* withdraw: function removeMoney (amount) {
                let verifyBalance = savingsAccount.balance - amount;
         if (amount > 0 && verifyBalance >= 0) {
             savingsAccount.balance -=amount;
         }
     },
+    */
+   /*
+    withdraw: function removeMoney () {
+        let verifyBalance = savingsAccount.balance - amount;
+ if (amount > 0 && verifyBalance >= 0) {
+    
+     savingsAccount.balance -=amount;
+ }
+},
+    
+
+
+    }
     printAccountSummary: function printAccountSummary(){
 
         return (
@@ -75,5 +135,42 @@ and your interest rate is ${savingsAccount.interestRatePercent}%`
 };
 
 console.log(savingsAccount.printAccountSummary());
+//document.write(savingsAccount.printAccountSummary());
 
-//document.write(savingsAccount.printAccountSummary);
+
+*/
+/*
+const facebookProfile = {
+    name: 'Vio',
+    friends: 0,
+    messages: [],
+
+postMessage: function (message) {
+    facebookProfile.messages.push(message);
+},
+deleteMessage: function (index) {
+    facebookProfile.messages.splice(index, 1)
+},
+addFriends: function () {
+    facebookProfile.friends +=1;
+},
+removeFriends: function () {
+    facebookProfile.friends = facebookProfile.friends - 1;
+}
+};
+
+console.log(facebookProfile.messages);
+facebookProfile.postMessage("My first name");
+facebookProfile.postMessage("Sveiki visi");
+console.log(facebookProfile.messages);
+
+facebookProfile.deleteMessage(0);
+console.log(facebookProfile.messages);
+
+console.log('Friends:', facebookProfile.friends);
+facebookProfile.addFriends();
+facebookProfile.addFriends();
+console.log('Friends:', facebookProfile.friends)
+
+*/
+
